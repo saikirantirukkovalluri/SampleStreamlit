b4eae05bfec2af3313c0c906f118a52b552dde49
SAI KIRAN <98216043+saikirantirukkovalluri@users.noreply.github.com>
2023-12-29 09:58:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,4 @@
 import streamlit as s
-s.write("Hello")
+s.write("<h1>Hello guys</h1>", unsafe_allow_html=True)
+s.write("<h1 style='color:red;'>hello</h1>,unsafe_allow_html=True)
+s.image("https://www.google.com/url?sa=i&url=https%3A%2F%2Fkmb.kerala.gov.in%2Fckeditor%2Fsamples%2Ffonts%2Fcricket%2Frohit-sharma-all-international-runs%2F&psig=AOvVaw271xtu63e6NrqY8ZqVdFAc&ust=1703910482832000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOjHoKvns4MDFQAAAAAdAAAAABAD")
